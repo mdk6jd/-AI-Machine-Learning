cf56a221a79d7544e26f2e305aa435e72ae4ffc8
kxie8 <kx3jj@virginia.edu>
2017-12-01 19:59:25 -0500

    fixed a bug in selectFeatures

--- machineLearning.py
+++ machineLearning.py
@@ -35,162 +35,6 @@ class Node:
 attributeValue = []
 
 
-# find the best feature to split the examples based on
-# best feature will be the ingredient that achieves the most even split
-def selectFeature(data, attrList, target_attr):
-	#cycle through each attribute and calculates info gain
-	#choose best gain
-	bestGain = sys.float_info.min
-	bestIngredient = ''
-	for ingredient in attrList:
-		infoGain = gain(data, ingredient, target_attr)
-		if infoGain > bestGain:
-			bestGain = infoGain
-			bestIngredient = ingredient
-	return ingredient
-	#return bestGain
-
-
-# data = test set (cuisine)
-def entropy(data, target_attr):
-	val_freq = {}
-	data_entropy = 0.0
-
-	#calculate frequency of each of the values in target attribute Value
-	#yes/no for ingredients, 20 different cuisine
-	#target_attr is cuisine and target_attr is ingredient
-	if (target_attr == 'cuisine'):
-		for dish in data:
-			if (dish.get('cuisine') in val_freq):
-				val_freq[ dish.get('cuisine') ] += 1.0
-			else:
-				val_freq[ dish.get('cuisine') ] = 1.0
-	else:
-		for dish in data:
-			hasIngredient = False
-			if target_attr in dish.get('ingredients'):
-				hasIngredient = True
-
-			if (hasIngredient in val_freq):
-				val_freq[ hasIngredient ] += 1.0
-			else:
-				val_freq[ hasIngredient ] = 1.0
-
-	#calculate entropty of data for target attribute, using S = Sum every attr value (p log p)
-	for freq in val_freq.values():
-		data_entropy += (-freq/len(data)) * math.log(freq/len(data), 2)
-
-	return data_entropy
-
-
-def gain(data, attr, target_attr):
-	#caculates info gain (reduction in entropy) by splitting data on chosen attribute (attr)
-	val_freq = {}
-	subset_entropy = 0.0
-
-	#calculate frequency of each values in attribute
-	if (attr == 'cuisine'):
-		for dish in data:
-			if (dish.get('cuisine') in val_freq):
-				val_freq[ dish.get('cuisine') ] += 1.0
-			else:
-				val_freq[ dish.get('cuisine') ] = 1.0
-	else:
-		for dish in data:
-			hasIngredient = False
-			if attr in dish.get('ingredients'):
-				hasIngredient = True
-			if (hasIngredient in val_freq):
-				val_freq[ hasIngredient ] += 1.0
-			else:
-				val_freq[ hasIngredient ] = 1.0
-
-	#calculate sum of entropy for each suset of dishes
-	for val in val_freq.keys():
-		val_prob = val_freq[val]/sum(val_freq.values())
-		data_subset = [dish for dish in data if (dish.get('cuisine') == val or attr in dish.get('ingredients') == val)]
-		subset_entropy += val_prob * entropy(data_subset,target_attr)
-
-	return (entropy(data, target_attr) - subset_entropy)
-
-
-
-# returns whether or not all examples are of the same category
-def sameCategory(examples):
-	category = examples[0]["cuisine"]
-	for example in examples:
-		if(not category == example["cuisine"]):
-			return False
-	return True
-
-def get_values(data, attr):
-	#cycles through each dish in data and return list of values for attribute
-	val_freq = {}
-	data_entropy = 0.0
-
-	for dish in data:
-		hasIngredient = False
-		if attr in dish.get('ingredients'):
-			hasIngredient = True
-		if (hasIngredient in val_freq):
-			val_freq[ hasIngredient ] += 1.0
-		else:
-			val_freq[ hasIngredient ] = 1.0
-	return val_freq.keys()
-
-
-def get_examples(data, best, val):
-	#returns list of dishes in data set that have val for the best attribute
-	dishResult = []
-	for dish in data:
-		hasIngredient = False
-		if best in dish.get('ingredients'):
-			hasIngredient = True
-		if hasIngredient == val:
-			dishResult.append(dish)
-	return dishResult
-
-
-def buildDecisionTree(data, attributes, target_attr):
-	#returns new decision tree based on the exmaples given
-	data = data [:]
-	#values of the cuisine
-	vals = [dish.get('cuisine') for dish in data]
-	default = {} #Node with Italian as value ?????????????????
-
-	#if there's no example set or if the ingredients list is empty
-	if not data or (len(attributes)-1) <= 0:
-		return default
-	#if all dish are same cuisine then return that classification
-	elif vals.count(vals[0]) == len(vals):
-		return vals[0]
-	else:
-		#choose next best attribute
-		best = selectFeature(data, attributes, target_attr)
-		#create new tree/node with best attribute and empty dictionary
-		tree = {best:{}}
-		#create new decision tree/sub node for each value in best attributes
-		#for val in get_values(data, best):
-		subtreeTrue = buildDecisionTree(
-			get_examples(data, best, True),
-			[a for a in attributes if a != best],
-			target_attr
-		)
-
-		#add new subtree to empty dictionary in our new tree/nodes
-		tree[best][True] = subtreeTrue
-
-		subtreeFalse = buildDecisionTree(
-			get_examples(data, best, False),
-			[a for a in attributes if a != best],
-			target_attr
-		)
-
-		#add new subtree to empty dictionary in our new tree/nodes
-		tree[best][False] = subtreeFalse
-	return tree
-
-
 
 # build the decision tree
 def buildTree(rootNode):
@@ -251,6 +95,157 @@ def testTree(rootNode, examples):
 	return numberIncorrect
 
 
+# find the best feature to split the examples based on
+# best feature will be the ingredient that achieves the most even split
+def selectFeature(data, attrList, target_attr):
+	#cycle through each attribute and calculates info gain
+	#choose best gain
+	bestGain = sys.float_info.min
+	bestIngredient = ''
+	for ingredient in attrList:
+		infoGain = gain(data, ingredient, target_attr)
+		if infoGain > bestGain:
+			bestGain = infoGain
+			bestIngredient = ingredient
+	return bestIngredient
+	#return bestGain
+
+
+# data = test set (cuisine)
+def entropy(data, target_attr):
+	val_freq = {}
+	data_entropy = 0.0
+
+	#calculate frequency of each of the values in target attribute Value
+	#yes/no for ingredients, 20 different cuisine
+	#target_attr is cuisine and target_attr is ingredient
+	#if (target_attr == 'cuisine'):
+	for dish in data:
+		if (dish.get('cuisine') in val_freq):
+			val_freq[ dish.get('cuisine') ] += 1.0
+		else:
+			val_freq[ dish.get('cuisine') ] = 1.0
+	#print(val_freq)
+
+	#calculate entropty of data for target attribute, using S = Sum every attr value (p log p)
+	for freq in val_freq.values():
+		if freq == 0:
+			continue
+		data_entropy += (-1.0*freq/len(data)) * math.log(1.0*freq/len(data), 2)
+	return data_entropy
+
+
+def gain(data, attr, target_attr):
+	#caculates info gain (reduction in entropy) by splitting data on chosen attribute (attr)
+	val_freq = {}
+	subset_entropy = 0.0
+
+	#calculate frequency of each values in attribute
+	for dish in data:
+		hasIngredient = False
+		if attr in dish.get('ingredients'):
+			hasIngredient = True
+		#print(hasIngredient)
+		#print(attr)
+		if (hasIngredient in val_freq):
+			val_freq[ hasIngredient ] += 1.0
+		else:
+			val_freq[ hasIngredient ] = 1.0
+
+	#calculate sum of entropy for each subset of dishes
+	for val in val_freq.keys():
+		#|Sv|/|S|
+		val_prob = 1.0*val_freq[val]/(1.0*sum(val_freq.values()))
+		#sublist of recipes with or without ingredient
+		data_subset = [dish for dish in data if ((attr in dish.get('ingredients')) == val)]
+		subset_entropy += val_prob * entropy(data_subset,target_attr)
+
+	#print(attr + " %f" %subset_entropy)
+	#print(attr + " %f" %(entropy(data, target_attr) - subset_entropy))
+	return (entropy(data, target_attr) - subset_entropy)
+
+
+
+# returns whether or not all examples are of the same category
+def sameCategory(examples):
+	category = examples[0]["cuisine"]
+	for example in examples:
+		if(not category == example["cuisine"]):
+			return False
+	return True
+
+def get_values(data, attr):
+	#cycles through each dish in data and return list of values for attribute
+	val_freq = {}
+	data_entropy = 0.0
+
+	for dish in data:
+		hasIngredient = False
+		if attr in dish.get('ingredients'):
+			hasIngredient = True
+		if (hasIngredient in val_freq):
+			val_freq[ hasIngredient ] += 1.0
+		else:
+			val_freq[ hasIngredient ] = 1.0
+	return val_freq.keys()
+
+
+def get_examples(data, best, val):
+	#returns list of dishes in data set that have val for the best attribute
+	dishResult = []
+	for dish in data:
+		hasIngredient = False
+		if best in dish.get('ingredients'):
+			hasIngredient = True
+		if hasIngredient == val:
+			dishResult.append(dish)
+	return dishResult
+
+
+def buildDecisionTree(data, attributes, target_attr):
+	#returns new decision tree based on the exmaples given
+	data = data [:]
+	#values of the cuisine
+	vals = [dish.get('cuisine') for dish in data]
+	default = {} #Node with Italian as value ?????????????????
+
+	#if there's no example set or if the ingredients list is empty
+	if not data or (len(attributes)-1) <= 0:
+		return default
+	#if all dish are same cuisine then return that classification
+	elif vals.count(vals[0]) == len(vals):
+		#print(vals[0])
+		return vals[0]
+	else:
+		#choose next best attribute
+		best = selectFeature(data, attributes, target_attr)
+		#print(best)
+		#create new tree/node with best attribute and empty dictionary
+		tree = {best:{}}
+		#create new decision tree/sub node for each value in best attributes
+		#for val in get_values(data, best):
+		subtreeTrue = buildDecisionTree(
+			get_examples(data, best, True),
+			[a for a in attributes if a != best],
+			target_attr
+		)
+
+		#add new subtree to empty dictionary in our new tree/nodes
+		tree[best][True] = subtreeTrue
+
+		subtreeFalse = buildDecisionTree(
+			get_examples(data, best, False),
+			[a for a in attributes if a != best],
+			target_attr
+		)
+
+		#add new subtree to empty dictionary in our new tree/nodes
+		tree[best][False] = subtreeFalse
+	return tree
+
+
+
+
 def main():
 	# import ingredients
 	ingredients = []
@@ -266,7 +261,8 @@ def main():
 
 
 	# split training set into 6 subsets (to use k-Fold cross validation on)
-	subset1 = training[0:100]
+	#subset1 = training[50:100]
+	subset1 = training[0:299]
 	subset2 = training[299:598]
 	subset3 = training[598:897]
 	subset4 = training[897:1196]
@@ -291,7 +287,10 @@ def main():
 	# repeat above code k-1 times
 
 	#print( selectFeature(training, ingredients, 'cuisine') )
-	print(buildDecisionTree(subset1, ingredients, "cuisine"))
+	buildDecisionTree(trainingSubset, ingredients, "cuisine")
+	#entropy(trainingSubset, "cuisine")
+	#ingredients2 = ["salt"]
+	#selectFeature(trainingSubset, ingredients, "cuisine")
 
 if __name__ == "__main__":
 	main()
